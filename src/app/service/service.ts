b8e98c16ac2464f525a92e733ccff12dd4071b69
Justin V <j.valenzuela28@live.com>
2018-01-07 22:55:44 -0700

    Add json data for users and roles and add get methods to the service

--- src/app/service/service.ts
+++ src/app/service/service.ts
@@ -11,12 +11,24 @@ import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 @Injectable()
 export class DataService {
-    private _productUrl = ' ../../../api/products.json'
+    private _productUrl = ' ../../../api/products.json';
+    private _rolesUrl = ' ../../../api/roles.json'
+    private _usersUrl = ' ../../../api/users.json'
 
     constructor(private _http: HttpClient) {}
     
     getProducts(): Observable<IProduct[]> {
         return this._http.get<IProduct[]>(this._productUrl)
+                    .catch(this.handleError);
+    }
+
+    getRoles(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._rolesUrl)
+                    .catch(this.handleError);
+    }
+
+    getUsers(): Observable<IProduct[]> {
+        return this._http.get<IProduct[]>(this._usersUrl)
                     .catch(this.handleError);
     }
 
